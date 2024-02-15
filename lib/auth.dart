8bbe77e136fd08246a8d29f3cb6295abd2d39d80
davidsomekh <67224401+davidsomekh@users.noreply.github.com>
2024-02-15 13:58:57 +0200

    add firebase auth

---
+++ lib/auth.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+class Auth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future appleLogin() async {
+    final appleProvider = AppleAuthProvider();
+    appleProvider.addScope('email');
+    // appleProvider.addScope('FULL_NAME');
+
+    if (kIsWeb) {
+      await FirebaseAuth.instance.signInWithPopup(appleProvider);
+    } else {
+      await FirebaseAuth.instance.signInWithProvider(appleProvider);
+    }
+  }
+
+  void deleteAccount() {
+    FirebaseAuth.instance.currentUser?.delete();
+  }
+
+  Future<void> sendPasswordResetEmail({
+    required String email,
+  }) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> passReset({required String email}) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
